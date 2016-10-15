8b1f00a2d85d21d9d2fdf9944890bbb870edaa65
TheStrix <parthbhatia98@gmail.com>
2016-10-15 13:42:54 +0530

    ido: Add XiaomiDoze package
    * Update doze overlays
    * Package kanged from OnePlus3 (OnePlusDoze -> XiaomiDoze)
    * Squashed commit of:
    
    	oneplus3: add doze package
    	Taken from yu lettuce
    	Includes the following patches
    
    	YUDoze: Add service for using proximity sensor for wake-up
    	Author: Patrick Lower <devvortex@gmail.com>
    	Date:   Mon May 11 16:14:17 2015 -0400
    
    	YUDoze: Set gesture to disabled by default
    	Author: Arne Coucheron <arco68@gmail.com>
    	Date:   Wed May 20 00:56:19 2015 +0200
    
    	YUDoze: Consolidate doze settings
    	Author: Christopher R. Palmer <crpalmer@gmail.com>
    	Date:   Sat May 23 11:42:49 2015 -0700
    
    	YUDoze: fix defaults
    	Author: Dave Daynard <nardholio@gmail.com>
    	Date:   Sat May 23 18:29:13 2015 -0400
    
    	YUDoze: Add back button to action bar
    	Author: luca020400 <luca.stefani.ge1@gmail.com>
    	Date:   Sun May 24 15:47:17 2015 -0700
    
    	YUDoze: add proximity wake
    	Author: Dave Daynard <nardholio@gmail.com>
    	Date:   Mon May 25 12:54:00 2015 -0400
    
    	YUDoze: restore proximity wake preference on boot
    	Author: Dave Daynard <nardholio@gmail.com>
    	Date:   Sat Jul 4 13:44:53 2015 -0400
    
    	YUDozeService: Prevent a pm wakeup with a timestamp in the future
    	Author: Brandon McAnsh <brandon.mcansh@gmail.com>
    	Date:   Fri Jul 24 15:40:45 2015 -0400
    
    	YUDozeService: properly fix future timestamp error
    	Author: Patrick Lower <devvortex@gmail.com>
    	Date:   Wed Aug 5 10:51:03 2015 -0400
    
    	YUDoze: fix typo
    	Author: Dave Daynard <nardholio@gmail.com>
    	Date:   Tue Sep 8 22:56:10 2015 -0400
    
    	YUDoze: Use ScreenType util from cmsdk
    	Author: Michael Bestas <mikeioannina@gmail.com>
    	Date:   Sun Dec 13 02:51:09 2015 -0500
    
    	YUDOze: Remove summary for YUDoze
    	Author: TheStrix <parthbhatia98@gmail.com>
    	Date:   Sun Dec 13 07:25:36 2015 -0500
    
    	oneplus2: doze improvements
    	Author:   Altaf-Mahdi <altaf.mahdi@gmail.com>
    	Date:     2016-04-24 23:01:57 +0100
    
    	op3: doze: clean up
    	Author:   Altaf-Mahdi <altaf.mahdi@gmail.com>
    	Date:     2016-08-21 08:47:12 +0100
    
    Change-Id: Ie2960d7f3897dd7f77d31c31813972a42c230c3b
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -113,6 +113,18 @@ PRODUCT_PACKAGES += \
     memtrack.msm8916 \
     liboverlay
 
+<<<<<<< HEAD
+=======
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprint.msm8916 \
+    fingerprintd
+
+>>>>>>> 9fc2859... ido: Add XiaomiDoze package
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
